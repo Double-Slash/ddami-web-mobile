b7c79358f7facab3d1e2929435f72bf85237cf52
hwangbo <taekwonod714@naver.com>
2020-07-20 19:52:54 +0900

    Feat: Navbar 기능 구현

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    const [open, setOpen] = useState(false);
+
+    const toggleSidebar = (event) => {
+        let key = `${event.currentTarget.parentNode.id}Open`;
+        this.setState({ [key]: !this.state[key] });
+    };
+
+    // TODO: open 상태에 따른 메뉴의 className 선언
+
+    return (
+        <div>
+            <div id="open" className="sidebar-icon">
+                <div className="icon" onClick={toggleSidebar}>
+                    ≡
+                </div>
+                {/* TODO: 메뉴 들어갈 자리 */}
+            </div>
+            <div className="logo">
+                <Link to="/">따미 로고</Link>
+            </div>
+            <div className="search-icon">
+                <Link to="/search">
+                    <img src="https://img.icons8.com/android/24/000000/search.png" alt="search-icon"/>
+                </Link>
+            </div>
+            <div className="user-icon">
+                알림
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
