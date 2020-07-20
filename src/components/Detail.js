49bdf7dd4cea07fbdf05e241d46482db0667b1ff
kmin-jeong <desk535353@gmail.com>
2020-07-20 21:39:49 +0900

    Fix : git conflict

---
+++ src/components/Detail.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title =styled.input`
+width: 91%;
+padding-bottom: 30px;
+border: none;
+font-size: 25px;
+border-bottom: solid 1px #ababab;
+font-weight: bold;
+`;
+const Texting = styled.textarea`
+width: 91%;
+resize: none;
+border: none;
+height: 500px;
+`;
+function Detail(){
+  return (
+        <div className="sangse">
+           <Title type="text" placeholder="제목"></Title> 
+           <Texting placeholder="내용을 입력하세요"></Texting>    
+        </div>
+    );
+}
+
+export default Detail;
+
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Bottom = styled.bottom`
+width: 90%;
+resize: none;
+border: none;
+height: 300px;
+`;
+
+const Footer = () => (
+    <Bottom>
+        <link to>따미 소개</link>|<link to>이용약관</link>|<link to>개인정보 처리방침</link>|
+        <link to>자주 묻는 질문</link>|<link to>고객센터</link>|<link to>재휴 문의</link>|
+        copyRight DDami Inc. All Rights Reserved
+    </Bottom>    
+)
+
+export default Footer;
