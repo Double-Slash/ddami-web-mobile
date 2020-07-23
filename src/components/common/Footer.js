34f299e500e6472216fb3e63587d83b6b310e13a
sbyeol3 <ahnsbyeol3@gmail.com>
2020-07-23 23:24:23 +0900

    Style : css 수정

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,16 +3,16 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 
 const Bottom = styled.div`
-width: 90%;
-resize: none;
-border: none;
-height: 300px;
+  width: 90%;
+  resize: none;
+  border: none;
+  height: 300px;
 `;
 
 const Footer = () => (
     <Bottom>
-        <Link to='/'>따미 소개</Link>|<Link to='/'>이용약관</Link>|<Link to='/'>개인정보 처리방침</Link>|
-        <Link to='/'>자주 묻는 질문</Link>|<Link to='/'>고객센터</Link>|<Link to='/'>재휴 문의</Link>|
+        <Link to='/introduce'>따미 소개</Link>|<Link to='/term'>이용약관</Link>|<Link to='/privacy'>개인정보 처리방침</Link>|
+        <Link to='/faq'>자주 묻는 질문</Link>|<Link to='/contact'>고객센터</Link>|<Link to='/contact'>제휴 문의</Link>|
         copyRight DDami Inc. All Rights Reserved
     </Bottom>    
 )
---
+++ src/components/workplace/WriteTopBar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const WriteTop = styled.div`
+  height: 60px;
+  padding: 0px 10px;
+  display: flex;
+  button {
+    
+    background-color: white;
+    outline: none;
+    border: none;
+    .back {
+      
+    }
+    .register {
+      color: #3C3C3C;
+      font-size: 16px;
+    }
+  }
+`
+
+const BackButton = styled.button`
+  width: 10%;
+`
+
+const TemporarySave = styled.div`
+  width: 75%;
+  text-align: right;
+`
+
+const TempContent = styled.p`
+  border: solid 1px grey;
+  border-radius: 10px;
+  padding: 5px;
+`
+
+const RegisterButton = styled.button`
+  width: 15%;
+`
+
+export default ({history}) => {
+  const routeBack = () => {
+    history.push('/workplace')
+  }
+  return(
+    <WriteTop>
+      <BackButton onClick={routeBack} className='back'>X</BackButton>
+      <TemporarySave>
+        <TempContent>
+          <button>임시저장</button>
+          <span>3</span>
+        </TempContent>
+      </TemporarySave>
+      <RegisterButton className='register'>등록</RegisterButton>
+    </WriteTop>
+  )
+}
