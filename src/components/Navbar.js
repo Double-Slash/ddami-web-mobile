35cf47a01405a62becf538115067fd2a892f8e31
hwangbo <taekwonod714@naver.com>
2020-07-26 16:43:07 +0900

    Style: Navbar, styled-component를 활용한 css 적용

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,68 @@
 import React, {useState} from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import styled from 'styled-components';
+
+const NavigationBar = styled.div`
+display: inline-block;
+width: 100%;
+height: 2.5rem;
+vertical-align: center;
+`;
+
+const NavLeft = styled.div`
+display: block;
+float: left;
+`;
+
+const HamburgerIcon = styled.div`
+font-size: 1.5rem;
+margin: .25rem .75rem;
+`;
+
+const Logo = styled(Link)`
+display: block;
+color: #000000;
+margin: .75rem;
+text-decoration: none;
+`;
+
+const NavRight = styled.div`
+display: block;
+float: right;
+vertical-align: center;
+margin: .75rem;
+`;
 
 function Navbar() {
     const [open, setOpen] = useState(false);
 
     const toggleSidebar = (event) => {
         let key = `${event.currentTarget.parentNode.id}Open`;
-        this.setState({ [key]: !this.state[key] });
+        this.setState({[key]: !this.state[key]});
     };
 
     // TODO: open 상태에 따른 메뉴의 className 선언
 
     return (
-        <div>
-            <div id="open" className="sidebar-icon">
-                <div className="icon" onClick={toggleSidebar}>
+        <NavigationBar>
+            <NavLeft id="open">
+                <HamburgerIcon onClick={toggleSidebar}>
                     ≡
-                </div>
+                </HamburgerIcon>
                 {/* TODO: 메뉴 들어갈 자리 */}
-            </div>
-            <div className="logo">
-                <Link to="/">따미 로고</Link>
-            </div>
-            <div className="search-icon">
+            </NavLeft>
+            <NavLeft id="logo">
+                <Logo to="/">따미 로고</Logo>
+            </NavLeft>
+            <NavRight className="user-icon">
+                알림
+            </NavRight>
+            <NavRight className="search-icon">
                 <Link to="/search">
                     <img src="https://img.icons8.com/android/24/000000/search.png" alt="search-icon"/>
                 </Link>
-            </div>
-            <div className="user-icon">
-                알림
-            </div>
-        </div>
+            </NavRight>
+        </NavigationBar>
     );
 }
 
