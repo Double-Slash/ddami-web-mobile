e6d7ecc0f9f1ed8ff80b5105fc3e4ac6a4fa07d6
kmin-jeong <desk535353@gmail.com>
2020-08-15 14:58:27 +0900

    fix: add button

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -23,7 +23,6 @@ width: 298px;
 height: 51px;
 opacity: 1;
 margin-bottom : 2.0em;
-
 `;
 
 const passwordInputt=`
@@ -42,6 +41,21 @@ letter-spacing: 0px;
 color: #8B8B8B;
 opacity: 1;
 `;
+
+const Button=`
+background: #3E309D 0% 0% no-repeat padding-box;
+border-radius: 25px;
+opacity: 1;
+top: 518px;
+left: 169px;
+width: 41px;
+height: 17px;
+text-align: right;
+font: Bold 15px/17px NanumSquare;
+letter-spacing: 0px;
+color: #E4E4E4;
+opacity: 1;
+`;
 function Login(){
     return(
         <div name="header">
@@ -51,6 +65,7 @@ function Login(){
             비밀번호
             <passwordInputt type = "password" name = "password"/>  
             <Forgot>비밀번호를 잊으셨나요?</Forgot>
+            <Button type="submit">회원가입</Button>
           </LoginForm>
         </div>
     );
